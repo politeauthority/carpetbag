24d50724246f008198a47aef88661ddd18c60540
Alix Fullerton <fullertonalix0@gmail.com>
2018-09-21 17:46:07 -0600

    Reworking and adding tests, reworking the meet of scrapy, no interface changes but added json_date static method

--- tests/test_base_scrapy.py
+++ tests/test_base_scrapy.py
@@ -2,12 +2,21 @@
 Run by using "pytest ." in the project root.
 
 """
+from datetime import datetime
+import os
 
 from scrapy import Scrapy
 from scrapy import user_agent
 
+import vcr
 
-class TestScrapy(object):
+
+CASSET_DIR = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    'data/vcr_cassettes')
+
+
+class TestBaseScrapy(object):
 
     def test___init__(self):
         """
@@ -15,20 +24,33 @@ class TestScrapy(object):
 
         """
         s = Scrapy()
-        s.proxies = {}
-        s.headers = {}
-        s.user_agent = ''
-        s.skip_ssl_verify = True
-        s.change_user_agent_interval = 10
-        s.outbound_ip = None
-        s.request_attempts = {}
-        s.request_count = 0
-        s.request_total = 0
-        s.last_request_time = None
-        s.last_response = None
-        s.send_user_agent = ''
-        s.max_content_length = 200000000
-        s.mininum_wait_time = 0
+        assert s.proxies == {}
+        assert s.headers == {}
+        assert s.user_agent == ''
+        assert s.skip_ssl_verify
+        assert s.change_user_agent_interval == 10
+        assert not s.outbound_ip
+        assert s.request_attempts == {}
+        assert s.request_count == 0
+        assert s.request_total == 0
+        assert not s.last_request_time
+        assert not s.last_response
+        assert s.send_user_agent == ''
+        assert s.max_content_length == 200000000
+        assert s.mininum_wait_time == 0
+        assert s.wait_and_retry_on_connection_error == 0
+        assert not s.username
+        assert not s.password
+        assert not s.auth_type
+
+    def test_json_date(self):
+        """
+        Tests the JSON date method to try and convert the information to JSON friendly output.
+
+        """
+        the_date = datetime(2018, 10, 13, 12, 12, 12)
+        assert Scrapy.json_date(the_date) == '2018-10-13 12:12:12'
+        assert isinstance(Scrapy.json_date(), str)
 
     def test_url_concat(self):
         """
@@ -38,25 +60,32 @@ class TestScrapy(object):
         assert Scrapy.url_concat('www.google.com', 'news') == 'www.google.com/news'
         assert Scrapy.url_concat('www.google.com', '/news') == 'www.google.com/news'
 
-    def test__request_attempts(self):
-        """
-        Tests that the request_attempts method is adding values to manifest var.
+    def test__make_request(self):
+        scraper = Scrapy()
+        with vcr.use_cassette(os.path.join(CASSET_DIR, 'test__make_request.yaml')):
+            scraper._make_request('GET', 'http://www.google.com/news')
 
-        """
-        s = Scrapy()
-        ret = s._request_attempts('http://google.com/news')
-        assert 'http://google.com/news' in ret['urls']
-        assert ret['urls']['http://google.com/news']['count'] == 1
+    # def test__request_attempts(self):
+    #     """
+    #     Tests that the request_attempts method is adding values to manifest var.
 
-    def test__set_headers(self):
+    #     """
+    #     s = Scrapy()
+    #     ret = s._request_attempts('http://google.com/news')
+    #     assert 'http://google.com/news' in ret['urls']
+    #     assert ret['urls']['http://google.com/news']['count'] == 1
+
+    def test__get_headers(self):
         """
         Tests that headers can be set by the scrapy application, and by the end-user.
 
         """
         s = Scrapy()
         s.headers = {'Content-Type': 'application/html'}
-        set_headers = s._set_headers(attempts={})
+        s.user_agent = "Mozilla/5.0 (Windows NT 10.0)"
+        set_headers = s._get_headers()
         assert set_headers['Content-Type'] == 'application/html'
+        assert set_headers['User-Agent'] == "Mozilla/5.0 (Windows NT 10.0)"
 
     def test__setup_proxies(self):
         """
