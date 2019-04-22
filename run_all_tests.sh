d29c0a6793306ad120a0104bc3781f52bb2abf7c
Alix <fullertonalix0@gmail.com>
2019-04-21 19:11:24 -0600

    Removing old dockerfiless (#63)

--- run_all_tests.sh
+++
@@ -1,9 +0,0 @@
-set -e
-docker-compose build --no-cache
-docker-compose up -d
-docker exec -it carpetbag_carpetbag_1 python3 setup.py build
-docker exec -it carpetbag_carpetbag_1 python3 setup.py install
-docker exec -it carpetbag_carpetbag_1 pytest .
-docker exec -it carpetbag_carpetbag_1 flake8
-docker-compose stop
-docker-compose rm
