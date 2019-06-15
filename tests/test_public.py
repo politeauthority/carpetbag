a2bb103d43835947c564ddd4bd3078b5bdca9134
Alix Fullerton <fullertonalix0@gmail.com>
2019-06-15 01:01:21 -0600

    Expirement n shit:

--- tests/test_public.py
+++ tests/test_public.py
@@ -15,7 +15,7 @@ from carpetbag import carpet_tools as ct
 
 TOR_PROXY_CONTAINER = os.environ.get("TOR_PROXY_CONTAINER", "tor")
 # UNIT_TEST_URL = os.environ.get("BAD_ACTOR_URL", "https//bas.bitgel.com")
-UNIT_TEST_URL = "https://bas.bitgel.com/"
+UNIT_TEST_URL = "http://bas.bitgel.com/"
 UNIT_TEST_URL_BROKEN = "http://0.0.0.0:90/"
 UNIT_TEST_AGENT = "CarpetBag v%s/ UnitTests" % CarpetBag.__version__
 
